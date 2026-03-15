c5a76330fe6290cd8c74c9e839dfbacc5407a732
Sanaa-Uzuri Robotham <sanaauzurirm@gmail.com>
2026-03-15 11:57:54 -0400

    Complete/fixed code

--- poke-catch/src/fetch-helpers.js
+++ poke-catch/src/fetch-helpers.js
@@ -1,38 +1,35 @@
 const getRandomPokemon = async () => {
-    try {
-        const randomId = Math.floor(Math.random() * 150) + 1;
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
-        if (!response.ok) { 
-            throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
+  try {
+    const randomId = Math.floor(Math.random() * 150) + 1;
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+    
+    if (!response.ok) {
+      throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
     }
-        const data = await response.json();
-        const pokemonObj = {
-        name: data.name,
-        types: data.types.map((typeObj) => typeObj.type.name).join(", "),
-        sprite: data.sprites.front_default
+    
+    const data = await response.json();
+    const pokemonObj = {
+      name: data.name,
+      types: data.types.map((typeObj) => typeObj.type.name).join(", "),
+      sprite: data.sprites.front_default
     };
-    return { 
-        data: pokemonObj, 
-        error: null };
-    } catch (error) {
-        return {
-            data: null, 
-            error};
-    }
+    
+    return { data: pokemonObj, error: null };
+  } catch (error) {
+    return { data: null, error };
+  }
 };
 
 const postDiscoveredPokemon = async (formData) => {
   try {
-    const config = {
+    const response = await fetch('https://formspree.io/f/xeerlpdg', {
       method: 'POST',
       body: JSON.stringify(formData),
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       }
-    };
-
-    const response = await fetch('https://formspree.io/f/xpqyjolo', config);
+    });
 
     if (!response.ok) {
       throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
